b091216d59cd2184c602ae812447014e0217dd29
Rob Warner <rwarner@grailbox.com>
2023-01-19 19:57:10 -0500

    feat: add message board with rotating messages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const defaultMessage = "Greetings from your fridge door";
+  const [sinceID, setSinceID] = useState(0);
+  const [currentMessage, setCurrentMessage] = useState(defaultMessage);
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      await updateMessage();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  });
+
+  const updateMessage = async () => {
+    const message = await getMessage();
+    setSinceID(message? message.id : 0);
+    setCurrentMessage(message? message.text: defaultMessage);
+  }
+
+  const getMessage = async () => {
+    const response = await fetch(
+      `http://127.0.0.1:8000/messages?count=1&since_id=${sinceID}`,
+      {
+        mode: "cors",
+      }
+    );
+    const messages = await response.json();
+    return messages ? messages[0] : null;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {currentMessage}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
